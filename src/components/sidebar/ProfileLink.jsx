2a4f91a7d39337bb359c519ac6b39473815f050a
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2025-12-26 20:50:25 +0100

    add vercel.json

--- src/components/sidebar/ProfileLink.jsx
+++ src/components/sidebar/ProfileLink.jsx
@@ -7,21 +7,21 @@ const ProfileLink = () => {
   if (!user || !user.fullName) return null;
 
   const nameParts = user.fullName.trim().split(/\s+/); 
-  // const firstName = nameParts[0] || "";
-  // const secondName = nameParts[1] || "";
+  const firstName = nameParts[0] || "";
+  const secondName = nameParts[1] || "";
 
-  // const firstInitial = firstName.charAt(0) || "";
-  // const secondInitial = secondName.charAt(0) || "";
+  const firstInitial = firstName.charAt(0) || "";
+  const secondInitial = secondName.charAt(0) || "";
 
 
   return (
     <div className="flex items-center gap-4 border-t-2 h-20 justify-center border-gray-400 text-black">
       <Avatar className={"w-14 h-14"}>
         <AvatarImage src="" alt="@shadcn" />
-        <AvatarFallback className={"text-white text-lg"}></AvatarFallback>
+        <AvatarFallback className={"text-white text-lg"}>{firstInitial + secondInitial}</AvatarFallback>
       </Avatar>
       <div className="truncate">
-        <h3 className="font-bold text-lg"></h3>
+        <h3 className="font-bold text-lg">{firstName}</h3>
         <p className="text-gray-600 truncate">{user?.email}</p>
       </div>
     </div>
